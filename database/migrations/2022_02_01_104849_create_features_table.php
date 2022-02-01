0a060562b633e0929aa32de2be23191d7bea7684
shiraishigumi01 <shiraishigumi01@gmail.com>
2022-02-01 10:50:59 +0900

    add: マイグレーションファイルを作成（再）
    works、features、tasksテーブルを作成するマイグレーションファイルを作成

---
+++ database/migrations/2022_02_01_104849_create_features_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('work_id');
+            $table->string('name');
+            $table->text('comment')->nullable();
+            $table->boolean('is_done');
+            $table->timestamps();
+
+            $table->foreign('work_id')
+                ->references('id')
+                ->on('works')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('features');
+    }
+}
